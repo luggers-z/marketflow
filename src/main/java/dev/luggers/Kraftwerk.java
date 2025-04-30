543fc3080fe8659fb6c9ed1af79cebac7e912b78
Thomas Hanel <thomas.m.hanel@gmail.com>
2025-04-30 19:45:24 +0200

    pimped pom.xml: added site and report support; added junit tests

--- src/main/java/dev/luggers/Kraftwerk.java
+++ src/main/java/dev/luggers/Kraftwerk.java
@@ -1,36 +1,89 @@
 package dev.luggers;
 
+/**
+ * Powerplant is ...
+ */
 public class Kraftwerk {
-    Kraftwerk nf;
-    Speicherbecken pool;
-    double wirkungsgrad;
-    double durchfluss;
-    double maxdurchfluss;
-    double mindurchfluss;
-    double hoehe;
+	public static final int KILOWATT_HOURS = 3600000;
 
+	private final String name;
+	private final Kraftwerk nf;
+	/*private*/ final Speicherbecken pool;
 
-    Kraftwerk(Kraftwerk nachfolger, Speicherbecken pool, double hoehe, double wirkungsgrad, double maxdurchfluss, double mindurchfluss) {
-        nf = nachfolger;
-        this.pool = pool;
-        this.hoehe = hoehe;
-        this.wirkungsgrad = wirkungsgrad;
-        this.maxdurchfluss = maxdurchfluss;
-        this.mindurchfluss = mindurchfluss;
-        durchfluss = 510;
-    }
-    public double fluss(double zufluss, double strompreis, double delta){
-        double fluss = (zufluss-durchfluss)*delta;
-        if(!pool.berechnen(fluss)) durchfluss = zufluss;
-        if (nf != null) {
-            return (gewinn(durchfluss, strompreis, delta) + nf.fluss(durchfluss, strompreis, delta));
-        } else {
-            return gewinn(durchfluss, strompreis, delta);
-        }
-    }
-    public double gewinn(double durchfluss,double strompreis, double delta){
-        double energie= hoehe*durchfluss*delta*1000*9.81*wirkungsgrad;
-        double kwh= energie/3600000;
-        return kwh*strompreis;
-    }
+	private double wirkungsgrad;
+	private double durchfluss;
+	private double maxdurchfluss;
+	private double mindurchfluss;
+	/*private*/ double hoehe;
+
+	/**
+	 * Create a new instance of a Powerplant, with the given capacity....
+	 * 
+	 * @param name          A String representing the human readable name of the
+	 *                      powerplant.
+	 * @param nachfolger    an instance of Powerplant, following down the line.
+	 *                      Maybe null.
+	 * @param pool          A reservoir attached to this Powerplant, never null.
+	 * @param hoehe
+	 * @param wirkungsgrad
+	 * @param maxdurchfluss
+	 * @param mindurchfluss
+	 */
+	public Kraftwerk(final String name, final Kraftwerk nachfolger, final Speicherbecken pool, final double hoehe,
+			final double wirkungsgrad, final double maxdurchfluss, double mindurchfluss) {
+		this.name = name;
+		this.nf = nachfolger;
+		this.pool = pool;
+		this.hoehe = hoehe;
+		this.wirkungsgrad = wirkungsgrad;
+		this.maxdurchfluss = maxdurchfluss;
+		this.mindurchfluss = mindurchfluss;
+		durchfluss = 510;
+	}
+
+	/**
+	 * Create a new instance of a Powerplant, with the given capacity, without a
+	 * downstream power plant.
+	 *
+	 * @param name          A String representing the human readable name of the
+	 *                      powerplant.
+	 * @param pool          A reservoir attached to this Powerplant, never null.
+	 * @param hoehe
+	 * @param wirkungsgrad
+	 * @param maxdurchfluss
+	 * @param mindurchfluss
+	 */
+	public Kraftwerk(final String name, final Speicherbecken pool, final double hoehe, final double wirkungsgrad,
+			final double maxdurchfluss, double mindurchfluss) {
+		this(name, null, pool, hoehe, wirkungsgrad, maxdurchfluss, mindurchfluss);
+	}
+
+	public double fluss(final double zufluss, final double strompreis, final double delta) {
+		double fluss = (zufluss - durchfluss) * delta;
+		if (!pool.berechnen(fluss)) {
+			durchfluss = zufluss;
+		}
+		final var gewinn = gewinn(durchfluss, strompreis, delta);
+
+		if (nf != null) {
+			return (gewinn + nf.fluss(durchfluss, strompreis, delta));
+		} else {
+			return gewinn;
+		}
+	}
+
+	public double gewinn(double durchfluss, double strompreis, double delta) {
+		double energie = hoehe * durchfluss * delta * 1000 * 9.81 * wirkungsgrad;
+		double kwh = energie / KILOWATT_HOURS;
+		return kwh * strompreis;
+	}
+
+	/**
+	 * Get the human readable name of the power plant.
+	 * 
+	 * @return A String representing the human readable name of the power plant.
+	 */
+	public String getName() {
+		return name;
+	}
 }
--- src/main/java/dev/luggers/Speicherbecken.java
+++ src/main/java/dev/luggers/Speicherbecken.java
@@ -1,51 +1,51 @@
 package dev.luggers;
 
 public class Speicherbecken {
-    double l;
-    double b;
-    double h;
-    double vol;
-    double minh;
-    double maxh;
-    Speicherbecken nf;
-    Speicherbecken(Speicherbecken nachfolger, double l, double b, double h,double minh, double maxh) {
-        nf = nachfolger;
-        this.l = l;
-        this.b = b;
-        this.h = h;
-        this.vol = l*b*h;
-        this.minh = minh;
-        this.maxh = maxh;
-    }
-    public boolean berechnen(double fluss){
-        double h1= (vol+fluss)/b/l;
-        if(h1>=minh) {
-            if(h1<=maxh){
-                vol += fluss;
-                h = vol / b / l;
-                return true;
-            }
-            else {
-                if(nf!=null) {
-                    nf.ueberfluss(fluss);
-                }
-                return true;
-            }
-        }
-        else{
-            return false;
-        }
-    }
-    public void ueberfluss(double fluss){
-        double h1= (vol+fluss)/b/l;
-        if(h1<=maxh) {
-            vol += fluss;
-            h = vol / b / l;
-        }
-        else {
-            if(nf!=null){
-                nf.ueberfluss(fluss);
-            }
-        }
-    }
+	double l;
+	double b;
+	double h;
+	double vol;
+	double minh;
+	double maxh;
+	Speicherbecken nf;
+
+	Speicherbecken(Speicherbecken nachfolger, double l, double b, double h, double minh, double maxh) {
+		nf = nachfolger;
+		this.l = l;
+		this.b = b;
+		this.h = h;
+		this.vol = l * b * h;
+		this.minh = minh;
+		this.maxh = maxh;
+	}
+
+	public boolean berechnen(double fluss) {
+		double h1 = (vol + fluss) / b / l;
+		if (h1 >= minh) {
+			if (h1 <= maxh) {
+				vol += fluss;
+				h = vol / b / l;
+				return true;
+			} else {
+				if (nf != null) {
+					nf.ueberfluss(fluss);
+				}
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public void ueberfluss(double fluss) {
+		double h1 = (vol + fluss) / b / l;
+		if (h1 <= maxh) {
+			vol += fluss;
+			h = vol / b / l;
+		} else {
+			if (nf != null) {
+				nf.ueberfluss(fluss);
+			}
+		}
+	}
 }
